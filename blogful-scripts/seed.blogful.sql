d46e1369d12bf62d8ce29fa75213cc8d30b19f73
Vincent Cataldo <vincentcataldo@me.com>
2020-08-25 12:32:43 -0700

    final commit before squashing errors

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,18 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES 
+('Gardening a Guide', '2020-11-07 19:10:25' , 'Gardening Steps 1-10. Lets go!' ),
+('Star Wars: Episode 9 Review', '2020-12-21 1:10:25', 'For sure in my Top 3' ),
+('Anakin Skywalker: The Chosen One', '2005-12-20 2:10:24', 'The Phrophacy: Anakin Skywalker will bring a end to the sith and bring Balance to the Force' ),
+('Sith Lords', '2010-01-07 19:10:25', 'Dark side of the force is a pathway to many abilities some considered to be unatural' ),
+('Jedi Masters','1999-01-01 19:10:25', 'We finally got the jedi council we knew we would want!'),
+('Bounty Hunting: A guide. By Boba Fett', '1980-05-07 19:10:25', 'How I went from on the run and fatherless to the most feared bounty hunter around'),
+('The Millenium Falcon a love letter', '2008-08-07 19:10:25', 'Its the ship that made the Kessel run in less then 12 Parsecs'),
+('X wing manuvers', '2020-08-08 19:10:25', 'here are some important manuvers to use when in ship to ship combat'),
+('Gardening a updated Guide', '2018-11-07 19:10:25', 'Gardening Steps 1-10. Lets go!' ),
+('Star Wars: Episode 3 Review', '2019-12-20 19:10:25', 'For sure in my Top 3' ),
+('Luke Skywalker: The Chosen One', '2005-12-20 05:10:25', 'The Phrophacy: Luke Skywalker will bring a end to the sith and bring Balance' ),
+('Sith Lords 2', '2020-08-11 20:10:25', 'Dark side of the force is a pathway to many abilities some considered to be unatural' ),
+('Jedi Masters 2', '1999-01-01 19:10:25', 'We finally got the jedi council we knew we would want!'),
+('Bounty Hunting: A guide. By Boba Fett revised', '1980-05-04 14:10:25', 'How I went from on the run and fatherless to the most feared bounty hunter around'),
+('The Millenium Falcon a love letter revised', '2019-11-07 19:10:25', 'Its the ship that made the Kessel run in less then 12 Parsecs'),
+('X wing manuvers updated', '2020-08-08 14:10:25' ,'here are some important manuvers to use when in ship to ship combat');
--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = { 
-    DB_URL: process.env.DB_URL || "postgresql://dunder_mifflin@localhost/knex-practice"
+    DB_URL: process.env.DB_URL || "postgresql://dunder_mifflin@localhost/knex-practice",
+    TEST_DB_URL: process.env.TEST_DB_URL || "postgresql://dunder_mifflin@localhost/knex-practice-test"
 }
